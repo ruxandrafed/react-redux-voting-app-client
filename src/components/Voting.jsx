2678a3f4710c8f0e5e7f919e16cd6959a75e98a8
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-11 12:28:27 -0800

    Add and enable the pure render mixin and add tests for the component (should only update UI if the prop changes, not if we mutate prop data (which we assume is immutable)

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Winner from './Winner';
 import Vote from './Vote';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   render: function() {
     return <div>
       {this.props.winner ?
@@ -11,3 +13,21 @@ export default React.createClass({
     </div>;
   }
 });
+
+/* If we only use immutable data in component props, and
+write the component as a pure component, we can have React
+use a more efficient strategy for detecting changes in
+the props.
+
+This is done by applying the PureRenderMixin that
+is available as an add-on package. When this mixin is added to
+a component, it changes the way React checks for changes in the
+component's props (and state). Instead of a deep compare it does
+a shallow compare, which is much, much faster.
+
+The reason we can do this is that by definition, there can never
+be changes within immutable data structures. If the props of a
+component are all immutable values, and the props keep pointing
+to the same values between renders, there can be no reason to
+re-render the component, and it can be skipped completely.
+.*/
