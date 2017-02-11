6151b4fb70f074b775751b0af7de05b59025e2ed
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-11 12:47:51 -0800

    Update index.js and kickstart the router

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
-
-const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = <Route component={App}>
   <Route path="/" component={Voting} />
 </Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('app')
 );
 
