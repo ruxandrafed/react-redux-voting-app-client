430cd7f3ab78ee7be82077ef3759a71bd96dbc94
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-11 16:32:55 -0800

    Set up the middleware only to send actions with remote true to the server; update our action creaters with remote true only for VOTE

--- src/remote_action_middleware.js
+++ src/remote_action_middleware.js
@@ -1,6 +1,8 @@
 export default socket => store => next => action => {
 //  console.log('in middleware', action);
-  socket.emit('action', action);
+  if (action.meta && action.meta.remote) {
+    socket.emit('action', action);
+  }
   return next(action);
 }
 
