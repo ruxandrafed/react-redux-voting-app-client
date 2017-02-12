939e3884cf53181c340b105d57c3aa6905c742bd
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-11 16:29:58 -0800

    Emit an action event from the middleware

--- src/remote_action_middleware.js
+++ src/remote_action_middleware.js
@@ -1,5 +1,6 @@
 export default socket => store => next => action => {
-  console.log('in middleware', action);
+//  console.log('in middleware', action);
+  socket.emit('action', action);
   return next(action);
 }
 
