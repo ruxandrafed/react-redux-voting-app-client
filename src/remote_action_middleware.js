a135a05e5702c0efa4036d6463606140866d2bde
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-11 16:27:00 -0800

    Plug in the middleware to the redux store and log all actions

--- src/remote_action_middleware.js
+++ src/remote_action_middleware.js
@@ -1,3 +1,17 @@
 export default store => next => action => {
-
+  console.log('in middleware', action);
+  return next(action);
 }
+
+//export default function(store) {
+//  return function(next) {
+//    return function(action) {
+//
+//    }
+//  }
+//}
+
+/* This style of nesting single-argument functions is
+called currying. With the curried version we can call the
+outermost function once, and get a return value that
+"remembers" which store to use. */
